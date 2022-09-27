a14b0bdf3ed508a2afd68666d207ed1e56806879
jullemusten <hellstrom.julia@hotmail.com>
2022-09-27 16:42:22 +0200

    pandas

--- main.py
+++ main.py
@@ -4,14 +4,28 @@
 #for i in list:
 #    print("coolt" , i)
 import csv
-import pandas
+import pandas as pd
+from IPython.display import display
 
 #def get_csv_death():
 #    data = open("data_roadtrafficdeath.csv")
 #    type(data)
 #    csvreader = csv.reader(data, delimiter=',')
 
-def get_nodes():
+def get_roadtrafficdeath():
+    df_roadtafficdeath = pd.read_csv("data_roadtrafficdeath.csv")
+    #df_roadtafficdeath
+    display(df_roadtafficdeath.columns)
+    #columnn = df_roadtafficdeath.columns
+    #print(columnn)
+    #location_column = df_roadtafficdeath["Location"]
+    #print(location_column)
+
+    df_limit = pd.read_csv("")
+
+    return
+
+#def get_nodes():
     data = open("data_roadtrafficdeath.csv")
     type(data)
     csvreader = csv.reader(data, delimiter=',')
@@ -29,5 +43,15 @@ def get_nodes():
     maze.remove(maze[0])
     return maze
 
-print(get_nodes())
+#def for_loop(maze):
+    for i in maze:
+        print(i)
+
+def run():
+    #maze = get_nodes()
+    #for_loop(maze)
+    #return
+    get_roadtrafficdeath()
+
+run()
 
